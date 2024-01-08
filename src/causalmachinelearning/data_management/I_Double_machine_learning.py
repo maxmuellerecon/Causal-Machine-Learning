8f58c4bf79e292e2803abfd88f528387d3072a51
maxmuellerecon <max.mueller@uni-bonn.de>
2024-01-08 11:29:29 +0100

    Lesson_I.

--- src/causalmachinelearning/data_management/I_Double_machine_learning.py
+++ src/causalmachinelearning/data_management/I_Double_machine_learning.py
@@ -6,7 +6,6 @@ import numpy as np
 from matplotlib import pyplot as plt
 import seaborn as sns
 import os
-#from nb21 import cumulative_gain, elast
 import statsmodels.formula.api as smf
 from matplotlib import style
 from lightgbm import LGBMRegressor
@@ -88,7 +87,6 @@ def plot_debiased(df):
     return plt
 
 
-
 #2.) Reduce the variance from y +3.) obtain residuals for Y
 def denoise_outcome(train, train_pred):
     """Denoise the outcome"""
@@ -102,48 +100,61 @@ def denoise_outcome(train, train_pred):
                                 + train[Y].mean())
     return train_pred_y
 
-# #Visualize
-# def plot_debiase_denoised(df):
-#     np.random.seed(123)
-#     sns.scatterplot(data=df.sample(1000), x="price_res", y="sales_res", hue="weekday");
-#     plt.savefig(os.path.join(BLDFIG, "06_Scatterplot_Debiased_Denoised.png"))
-#     return plt.show()
-# plot_debiase_denoised(train_pred)
 
-# #4.) Use the orthogonalized residuals to estimate the causal effect (ATE), get negative relationship
-# final_model = smf.ols("sales_res~price_res", data=train_pred).fit()
-# final_model.summary()
-
-# #In coparison to unorthogonalized model, have positive relationship
-# basic_model = smf.ols("sales~price", data=train).fit()
-# basic_model.summary()
+def plot_debiased_denoised(df):
+    """Visualize debiased and denoised treatment"""
+    np.random.seed(123)
+    sns.scatterplot(data=df.sample(1000), x="price_res", y="sales_res", hue="weekday");
+    return plt
 
 
-# #####################I.3 Parametric Double ML CATE############################################
-# #Now we want to produce the CATE, not the ATE, interact the residuals with the covariates
-# final_model_cate = smf.ols(formula='sales_res ~ price_res * (temp + C(weekday) + cost)', data=train_pred).fit()
+#4.) Use the orthogonalized residuals to estimate the causal effect (ATE), get negative relationship
+def comparison_models(train_pred_y, train):
+    """Compare the models"""
+    final_model = smf.ols("sales_res~price_res", data=train_pred_y).fit().summary()
+    #In coparison to unorthogonalized model, have positive relationship
+    basic_model = smf.ols("sales~price", data=train).fit().summary()
+    return final_model, basic_model
 
-# #Use that model to predict the CATE
-# #Which is also called the R-Learner (Residualization)
-# cate_test = test.assign(cate=final_model_cate.predict(test.assign(price_res=1))
-#                         - final_model_cate.predict(test.assign(price_res=0)))
+
+# ######################I.3 Parametric Double ML CATE###############################################
+def parametric_double_ml_cate(train_pred_y, test):
+    """Parametric in the second stage (still linear model), before fully interacted"""
+    # #Now we want to produce the CATE, not the ATE, interact the residuals with the covariates
+    final_model_cate = smf.ols(formula='sales_res ~ price_res * (temp + C(weekday) + cost)', data=train_pred_y).fit()
+    #Use that model to predict the CATE
+    #Which is also called the R-Learner (Residualization)
+    cate_test = test.assign(cate=final_model_cate.predict(test.assign(price_res=1))
+                            - final_model_cate.predict(test.assign(price_res=0)))
+    return final_model_cate, cate_test
 
 
 # #####################I.4 Non-parametric Double ML CATE############################################
-# #We stil had linear specifications in the last step, now we want to use non-linear models
-# #Same as before:
-# debias_m = LGBMRegressor(max_depth=3)
-# denoise_m = LGBMRegressor(max_depth=3)
-# train_pred = train.assign(price_res =  train[T] - cross_val_predict(debias_m, train[X], train[T], cv=5),
-#                           sales_res =  train[Y] - cross_val_predict(denoise_m, train[X], train[Y], cv=5))
+#We still had linear specifications in the last step, now we want to use non-linear models
+#Same as before:
+def orthogonalize_treatment_and_outcome(train):
+    """orthogonalize_treatment_and_outcome."""
+    T = "price"
+    X = ["temp", "weekday", "cost"]
+    Y = "sales"
+    debias_m = LGBMRegressor(max_depth=3)
+    denoise_m = LGBMRegressor(max_depth=3)
+    train_pred_nonparam = train.assign(price_res =  train[T] - cross_val_predict(debias_m, train[X], train[T], cv=5),
+                            sales_res =  train[Y] - cross_val_predict(denoise_m, train[X], train[Y], cv=5))
+    return train_pred_nonparam
 
-# #Define a causal loss function, that the R-learner minimizes
-# model_final = LGBMRegressor(max_depth=3)
-# # create the weights
-# w = train_pred["price_res"] ** 2
-# # create the transformed target
-# y_star = (train_pred["sales_res"] / train_pred["price_res"])
-# # use a weighted regression ML model to predict the target with the weights.
-# model_final.fit(X=train[X], y=y_star, sample_weight=w)
-# #Estimate the treatment effect
-# cate_test_non_param = test.assign(cate=model_final.predict(test[X]))
+
+def non_parametric_double_ml_cate(train_pred_nonparam, test):
+    """Non-parametric in the second stage."""
+    X = ["temp", "weekday", "cost"]
+    #Define a causal loss function, that the R-learner minimizes
+    model_final = LGBMRegressor(max_depth=3)
+    # create the weights
+    w = train_pred_nonparam["price_res"] ** 2
+    # create the transformed target
+    y_star = (train_pred_nonparam["sales_res"] / train_pred_nonparam["price_res"])
+    # use a weighted regression ML model to predict the target with the weights.
+    model_final.fit(X=train_pred_nonparam[X], y=y_star, sample_weight=w)
+    #Estimate the treatment effect
+    cate_test_non_param = test.assign(cate=model_final.predict(test[X]))
+    return cate_test_non_param
--- src/causalmachinelearning/data_management/task_I_Double_machine_learning.py
+++ src/causalmachinelearning/data_management/task_I_Double_machine_learning.py
@@ -7,7 +7,7 @@ import numpy as np
 import pickle
 
 from causalmachinelearning.config import BLD, SRC
-from causalmachinelearning.data_management.I_Double_machine_learning import plot_pattern, fwl_theorem, verify_fwl_theorem, debias_treatment, plot_debiased, denoise_outcome
+from causalmachinelearning.data_management.I_Double_machine_learning import plot_pattern, fwl_theorem, verify_fwl_theorem, debias_treatment, plot_debiased, denoise_outcome, plot_debiased_denoised, comparison_models, parametric_double_ml_cate, orthogonalize_treatment_and_outcome, non_parametric_double_ml_cate
 
 
 def task_plot_pattern(
@@ -102,3 +102,94 @@ def task_denoise_outcome(
     train_pred = pd.read_csv(depends_on["train_pred"])
     train_pred_y = denoise_outcome(train, train_pred)
     train_pred_y.to_csv(produces["train_pred_y"], index=False)
+    
+
+def task_plot_debiased_denoised(
+    depends_on={
+        "train_pred_y": BLD / "python" / "Lesson_I" / "data" / "train_pred_y.csv",
+    },
+    produces={
+        "plot_debiased_denoised": BLD / "python" / "Lesson_I" / "figures" / "plot_debiased_denoised.png",
+    }
+    ):
+    """Plot debiased and denoised scatterplot"""
+    train_pred_y = pd.read_csv(depends_on["train_pred_y"])
+    plot = plot_debiased_denoised(train_pred_y)
+    plot.savefig(produces["plot_debiased_denoised"])
+    
+
+def task_comparison_models(
+    depends_on={
+        "train": SRC / "data" / "ice_cream_sales.csv",
+        "train_pred_y": BLD / "python" / "Lesson_I" / "data" / "train_pred_y.csv",
+    },
+    produces={
+        "final_model": BLD / "python" / "Lesson_I" / "tables" / "final_model.tex",
+        "basic_model": BLD / "python" / "Lesson_I" / "tables" / "basic_model.tex",
+    }
+    ):
+    """Compare the models"""
+    train = pd.read_csv(depends_on["train"])
+    train_pred_y = pd.read_csv(depends_on["train_pred_y"])
+    final_model, basic_model = comparison_models(train_pred_y, train)
+    # Export to LaTeX
+    latex_code = final_model.as_latex()
+    print(latex_code)
+    with open(produces["final_model"], 'w') as f:
+        f.write(latex_code)
+    # Append to LaTeX for basic model
+    latex_code_basic = basic_model.as_latex()
+    print(latex_code_basic)
+    with open(produces["basic_model"], 'a') as f:
+        f.write(latex_code_basic)
+        
+
+def task_parametric_double_ml_cate(
+    depends_on={
+        "test": SRC / "data" / "ice_cream_sales_rnd.csv",
+        "train_pred_y": BLD / "python" / "Lesson_I" / "data" / "train_pred_y.csv",
+    },
+    produces={
+        "final_model_cate": BLD / "python" / "Lesson_I" / "models" / "final_model_cate.pkl",
+        "cate_test": BLD / "python" / "Lesson_I" / "data" / "cate_test.csv",
+    }
+    ):
+    """Parametric Double ML CATE"""
+    test = pd.read_csv(depends_on["test"])
+    train_pred_y = pd.read_csv(depends_on["train_pred_y"])
+    final_model_cate, cate_test = parametric_double_ml_cate(train_pred_y, test)
+    #Save the model
+    with open(produces["final_model_cate"], "wb") as f:
+        pickle.dump(final_model_cate, f)
+    #Save the CATE
+    cate_test.to_csv(produces["cate_test"], index=False)
+    
+    
+def task_orthogonalize_treatment_and_outcome(
+    depends_on={
+        "train": SRC / "data" / "ice_cream_sales.csv",
+    },
+    produces={
+        "train_pred_nonparam": BLD / "python" / "Lesson_I" / "data" / "train_pred_nonparam.csv",
+    }
+    ):
+    """Orthogonalize treatment and outcome"""
+    train = pd.read_csv(depends_on["train"])
+    train_pred_nonparam = orthogonalize_treatment_and_outcome(train)
+    train_pred_nonparam.to_csv(produces["train_pred_nonparam"], index=False)
+    
+
+def task_non_parametric_double_ml_cate(
+    depends_on={
+        "test": SRC / "data" / "ice_cream_sales_rnd.csv",
+        "train_pred_nonparam": BLD / "python" / "Lesson_I" / "data" / "train_pred_nonparam.csv",
+    },
+    produces={
+        "cate_test_nonparam": BLD / "python" / "Lesson_I" / "data" / "cate_test_nonparam.csv",
+    }
+    ):
+    """Non-parametric Double ML CATE"""
+    test = pd.read_csv(depends_on["test"])
+    train_pred_nonparam = pd.read_csv(depends_on["train_pred_nonparam"])
+    cate_test_nonparam = non_parametric_double_ml_cate(train_pred_nonparam, test)
+    cate_test_nonparam.to_csv(produces["cate_test_nonparam"], index=False)
